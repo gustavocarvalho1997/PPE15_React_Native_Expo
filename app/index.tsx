f47afdba5a8a1d512796d2f5d1152d4e97d99fd9
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-05-11 20:04:43 -0300

    feat: add ButtonCard component and integrate it into the main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
-import { ScrollView } from "react-native";
+import { ScrollView, Text } from "react-native";
 
 import { StatusBar } from "expo-status-bar";
 import { Header } from "../components/Header";
+import { ButtonCard } from "../components/Button-card";
 
 export default function Screen() {
     return (
         <ScrollView className="h-screen bg-white">
             <Header />
+            <ButtonCard title="Conta" onPress={() => {}}>
+                <Text className="text-2xl font-semibold">R$ 1.200,00</Text>
+            </ButtonCard>
             <StatusBar style="auto" />
         </ScrollView>
     );
